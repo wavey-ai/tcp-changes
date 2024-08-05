318203971459649db79d7630344b175e23fd3de4
jbrough <jamie1612@gmail.com>
2024-08-05 16:40:20 +0100

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use bytes::{Buf, BufMut, Bytes, BytesMut};
-use crc32fast::Hasher;
 use rustls::pki_types::ServerName;
 use std::collections::BTreeMap;
+use std::net::Ipv4Addr;
 use std::net::SocketAddr;
 use std::sync::Arc;
 use std::sync::RwLock;
@@ -13,9 +13,26 @@ use tokio::time::Duration;
 use tokio_rustls::server;
 use tracing::{debug, error, info, warn};
 
+#[derive(Debug, Clone)]
+pub struct Message {
+    tag: String,
+    data: Vec<Bytes>,
+}
+
+impl Message {
+    pub fn new(tag: String, data: Vec<Bytes>) -> Self {
+        Message { tag, data }
+    }
+
+    pub async fn send(&self, chan: mpsc::Sender<Message>) {
+        chan.send(self.clone()).await.unwrap();
+    }
+}
+
 pub struct Payload {
     tag: String,
     val: Bytes,
+    ipv4: Ipv4Addr,
 }
 
 pub struct Client {
@@ -35,7 +52,7 @@ impl Client {
 
     pub async fn start(
         &self,
-        id: u64,
+        cmd: &str,
     ) -> Result<
         (
             oneshot::Receiver<()>,
@@ -56,7 +73,7 @@ impl Client {
         let domain = ServerName::try_from(dns).map_err(|_| "Invalid DNS name")?;
         let mut stream = connector.connect(domain, stream).await?;
 
-        stream.write_all(&id.to_be_bytes()).await?;
+        stream.write_all(&cmd.as_bytes()).await?;
 
         let srv = async move {
             up_tx.send(()).unwrap();
@@ -86,9 +103,12 @@ impl Client {
 
                                     if let Some(len) = current_frame_length {
                                         if buffer.len() >= len {
+                                            let mut ipv4_bytes = buffer.split_to(4);
+                                            let ipv4 = Ipv4Addr::from(ipv4_bytes.get_u32());
                                             let tag = buffer.split_to(4);
-                                            let val = buffer.split_to(len - 4);
+                                            let val = buffer.split_to(len - 8);
                                             let pkt = Payload {
+                                                ipv4,
                                                 tag: std::str::from_utf8(&tag).unwrap_or("invalid").to_string(),
                                                 val: val.freeze(),
                                             };
@@ -126,91 +146,17 @@ impl Client {
 }
 
 pub struct Server {
-    streams: Arc<RwLock<BTreeMap<u64, broadcast::Sender<Bytes>>>>,
-    changes: Arc<broadcast::Sender<Bytes>>,
     cert_pem: String,
     privkey_pem: String,
+    priv_ipv4: Ipv4Addr,
 }
 
 impl Server {
-    pub fn new(cert_pem: String, privkey_pem: String) -> Self {
-        let (tx, _) = broadcast::channel(16);
-
+    pub fn new(cert_pem: String, privkey_pem: String, priv_ipv4: Ipv4Addr) -> Self {
         Self {
-            streams: Arc::new(RwLock::new(BTreeMap::new())),
-            changes: Arc::new(tx),
             cert_pem,
             privkey_pem,
-        }
-    }
-
-    fn send(&self, id: u64, packet: Bytes) {
-        let mut inserted = false;
-        {
-            let mut lock = self.streams.write().unwrap();
-            let tx = lock.entry(id).or_insert_with(|| {
-                let (tx, _) = broadcast::channel(16);
-                inserted = true;
-                tx
-            });
-
-            match tx.send(packet) {
-                Ok(_) => {}
-                Err(e) => {
-                    eprintln!("error broadcasting: {}", e);
-                }
-            }
-        }
-
-        if inserted {
-            let keys: Vec<u64> = {
-                let lock = self.streams.read().unwrap();
-                lock.keys().cloned().collect()
-            };
-
-            let mut data = BytesMut::new();
-            data.put_u32((keys.len() * 8) as u32);
-            for key in keys {
-                data.put_u64(key);
-            }
-
-            match self.changes.send(data.freeze()) {
-                Ok(_) => {}
-                Err(e) => {
-                    eprintln!("error broadcasting: {}", e);
-                }
-            }
-        }
-    }
-
-    pub fn fin(&self, id: u64) {
-        let packet_size = 4 as u32;
-        let mut packet = BytesMut::with_capacity(4 + packet_size as usize);
-        packet.put_u32(packet_size);
-        packet.put_slice(b"fini");
-        self.send(id, packet.freeze());
-        self.streams.write().unwrap().remove(&id);
-    }
-
-    pub fn add(&self, id: u64, tag: &[u8], data: Vec<Bytes>) {
-        let mut packet_size = 4;
-        for d in &data {
-            packet_size += d.len();
-        }
-
-        {
-            let mut packet = BytesMut::with_capacity(4);
-            packet.put_u32(packet_size as u32);
-            self.send(id, packet.freeze())
-        }
-        {
-            let mut packet = BytesMut::with_capacity(4);
-            packet.put_slice(tag);
-            self.send(id, packet.freeze())
-        }
-
-        for d in data {
-            self.send(id, d);
+            priv_ipv4,
         }
     }
 
@@ -222,20 +168,22 @@ impl Server {
             oneshot::Receiver<()>,
             oneshot::Receiver<()>,
             watch::Sender<()>,
+            mpsc::Sender<Message>,
         ),
         Box<dyn std::error::Error + Send + Sync>,
     > {
         let (shutdown_tx, mut shutdown_rx) = watch::channel(());
         let (up_tx, up_rx) = oneshot::channel();
         let (fin_tx, fin_rx) = oneshot::channel();
+        let (tx, mut rx) = mpsc::channel::<Message>(16);
+        let (btx, _) = broadcast::channel(16);
 
         let tls_acceptor = tls_acceptor_from_base64(&self.cert_pem, &self.privkey_pem).unwrap();
         let incoming = TcpListener::bind(addr).await.unwrap();
         up_tx.send(()).unwrap();
 
-        let streams_clone = Arc::clone(&self.streams);
-        let changes_clone = Arc::clone(&self.changes);
-
+        let mut last = Arc::new(None);
+        let priv_ip = self.priv_ipv4.clone();
         let srv = async move {
             loop {
                 tokio::select! {
@@ -243,11 +191,12 @@ impl Server {
                         match result {
                             Ok((stream, _)) => {
                                 let tls_acceptor = tls_acceptor.clone();
-                                let streams_clone = Arc::clone(&streams_clone);
-                                let changes_clone = Arc::clone(&changes_clone);
+                                let rx = btx.subscribe();
+                                let last_copy = Arc::clone(&last);
+                                let priv_ip = priv_ip.clone();
                                 tokio::task::spawn(async move {
                                     let stream = tls_acceptor.accept(stream).await.unwrap();
-                                    stream_handler(stream, streams_clone, changes_clone).await;
+                                    stream_handler(stream, rx, last_copy, priv_ip.clone()).await;
                                 });
                             }
                             Err(err) => {
@@ -259,6 +208,10 @@ impl Server {
                         info!("Received shutdown signal, exiting...");
                         break;
                     }
+                    Some(message) = rx.recv() => {
+                        last = Arc::new(Some(message.clone()));
+                        btx.send(message);
+                    }
                 }
             }
 
@@ -269,25 +222,40 @@ impl Server {
 
         tokio::spawn(srv);
 
-        Ok((up_rx, fin_rx, shutdown_tx))
+        Ok((up_rx, fin_rx, shutdown_tx, tx))
     }
 }
 
 async fn stream_handler(
     mut stream: server::TlsStream<TcpStream>,
-    streams: Arc<RwLock<BTreeMap<u64, broadcast::Sender<Bytes>>>>,
-    changes: Arc<broadcast::Sender<Bytes>>,
+    mut rx: broadcast::Receiver<Message>,
+    last: Arc<Option<Message>>,
+    priv_ipv4: Ipv4Addr,
 ) {
-    let mut buffer = [0; 1024];
+    let mut buffer: [u8; 1024] = [0; 1024];
     match stream.read(&mut buffer).await {
         Ok(n) => {
             if let Ok(command) = String::from_utf8(buffer[..n].to_vec()) {
                 let command = command.trim().to_string();
-                if command == "LIST" {
-                    handle_list_command(&mut stream, &streams, changes).await;
-                } else {
-                    let id = u64::from_be_bytes(buffer[..8].try_into().unwrap());
-                    handle_stream_command(&mut stream, &streams, id).await;
+                if command == "HELLO" {
+                    if let Some(msg) = last.as_ref().as_ref() {
+                        send_message(&mut stream, msg.clone(), priv_ipv4).await;
+                    }
+                    loop {
+                        match rx.recv().await {
+                            Ok(message) => {
+                                send_message(&mut stream, message, priv_ipv4).await;
+                            }
+                            Err(broadcast::error::RecvError::Lagged(skipped)) => {
+                                warn!("Broadcast channel lagged, skipped {} messages", skipped);
+                                // Continue listening instead of breaking
+                            }
+                            Err(broadcast::error::RecvError::Closed) => {
+                                info!("Broadcast channel closed");
+                                break;
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -297,93 +265,51 @@ async fn stream_handler(
     }
 }
 
-async fn handle_list_command(
+async fn send_message(
     stream: &mut server::TlsStream<TcpStream>,
-    streams: &Arc<RwLock<BTreeMap<u64, broadcast::Sender<Bytes>>>>,
-    changes: Arc<broadcast::Sender<Bytes>>,
+    message: Message,
+    priv_ipv4: Ipv4Addr,
 ) {
-    let ids = {
-        let lock = streams.read().unwrap();
-        lock.keys().cloned().collect::<Vec<_>>()
-    };
-    let mut data = BytesMut::new();
-    data.put_u32((ids.len() * 8) as u32);
-    for id in &ids {
-        data.put_u64(*id);
-    }
-    info!("Sending initial changes {:?}", ids);
-    if let Err(e) = stream.write_all(&data).await {
-        error!("Error sending initial changes: {:?}", e);
-        return;
+    let tag = message.tag;
+    let data = message.data;
+
+    let mut packet_size = 8; // tag + ipv4
+    for d in &data {
+        packet_size += d.len();
     }
 
-    let mut rx = changes.subscribe();
-    loop {
-        match tokio::time::timeout(Duration::from_secs(30), rx.recv()).await {
-            Ok(result) => match result {
-                Ok(data) => {
-                    info!("Sending change {:?}", data);
-                    if let Err(e) = stream.write_all(&data).await {
-                        error!("Error sending change: {:?}", e);
-                        break;
-                    }
-                }
-                Err(broadcast::error::RecvError::Closed) => break,
-                Err(e) => {
-                    error!("Error receiving data from broadcast channel: {:?}", e);
-                    break;
-                }
-            },
-            Err(_) => {
-                debug!("Timeout waiting for changes, continuing...");
-                // Instead of breaking, we continue to wait for more changes
-            }
+    {
+        let mut packet = BytesMut::with_capacity(4);
+        packet.put_u32(packet_size as u32);
+        if let Err(e) = stream.write_all(&packet.freeze()).await {
+            error!("Error sending initial changes: {:?}", e);
+            return;
         }
     }
-}
 
-async fn handle_stream_command(
-    stream: &mut server::TlsStream<TcpStream>,
-    streams: &Arc<RwLock<BTreeMap<u64, broadcast::Sender<Bytes>>>>,
-    id: u64,
-) {
-    let tx = {
-        let lock = streams.read().unwrap();
-        lock.get(&id).cloned()
-    };
-
-    if let Some(tx) = tx {
-        let mut rx = tx.subscribe();
-        loop {
-            match tokio::time::timeout(Duration::from_secs(5), rx.recv()).await {
-                Ok(result) => match result {
-                    Ok(data) => {
-                        debug!("Writing data for stream {}: {:?}", id, data);
-                        if let Err(e) = stream.write_all(&data).await {
-                            error!("Error writing to stream {}: {:?}", id, e);
-                            break;
-                        }
-                    }
-                    Err(broadcast::error::RecvError::Lagged(skipped)) => {
-                        warn!(
-                            "Broadcast channel lagged for stream {}, skipped {} messages",
-                            id, skipped
-                        );
-                        // Continue listening instead of breaking
-                    }
-                    Err(broadcast::error::RecvError::Closed) => {
-                        info!("Broadcast channel closed for stream {}", id);
-                        break;
-                    }
-                },
-                Err(_) => {
-                    debug!("Timeout waiting for data on stream {}, continuing...", id);
-                    // Instead of breaking, we continue to wait for more data
-                }
-            }
+    {
+        let mut packet = BytesMut::with_capacity(4);
+        packet.put_slice(&priv_ipv4.octets());
+        if let Err(e) = stream.write_all(&packet.freeze()).await {
+            error!("Error sending initial changes: {:?}", e);
+            return;
         }
-    } else {
-        error!("No broadcast sender found for stream {}", id);
+    }
+
+    {
+        let mut packet = BytesMut::with_capacity(4);
+        packet.put_slice(tag.as_bytes());
+        if let Err(e) = stream.write_all(&packet.freeze()).await {
+            error!("Error sending initial changes: {:?}", e);
+            return;
+        }
+    }
+
+    for d in data {
+        if let Err(e) = stream.write_all(&d).await {
+            error!("Error sending initial changes: {:?}", e);
+            return;
+        }
     }
 }
 
@@ -399,27 +325,30 @@ mod tests {
         let privkey = env::var("PRIVKEY_PEM").unwrap();
         let ca_cert = env::var("FULLCHAIN_PEM").unwrap();
 
-        let mq = Server::new(cert, privkey);
+        let mq = Server::new(cert, privkey, Ipv4Addr::new(192, 168, 0, 1));
         let addr: SocketAddr = ([0, 0, 0, 0], 4243).into();
-        let (up, fin, shutdown) = mq.start(addr).await.unwrap();
+        let (up, fin, shutdown, tx) = mq.start(addr).await.unwrap();
         up.await.unwrap();
-        mq.add(1, b"abcd", vec![Bytes::from("foo")]);
-
+        let msg = Message::new(String::from("xxxx"), vec![Bytes::from("foo")]);
+        tx.send(msg).await;
         let mb = Client::new("local.wavey.io".to_string(), addr, ca_cert);
-        let (mb_up, mb_fin, mb_shutdown, mut rx) = mb.start(1).await.unwrap();
+        let (mb_up, mb_fin, mb_shutdown, mut rx) = mb.start("HELLO").await.unwrap();
         mb_up.await.unwrap();
         sleep(Duration::from_millis(100)).await;
 
-        // Test different packets with assertions for both tag and value
-        let test_cases = vec![(b"abcd", "foo"), (b"efgh", "bar"), (b"abcd", "baz")];
+        let test_cases = vec![("abcd", "foo"), ("efgh", "bar"), ("abcd", "baz")];
+        let payload = rx.try_recv().expect("expected data on channel");
+
+        assert_eq!(payload.tag, String::from("xxxx"), "Tag mismatch");
 
         for (tag, val) in test_cases.into_iter() {
-            mq.add(1, tag, vec![Bytes::from(val)]);
-            sleep(Duration::from_millis(10)).await;
-
+            let msg = Message::new(String::from(tag), vec![Bytes::from(val)]);
+            tx.send(msg).await;
+            sleep(Duration::from_millis(100)).await;
             let payload = rx.try_recv().expect("expected data on channel");
 
-            assert_eq!(payload.tag, String::from_utf8_lossy(tag), "Tag mismatch");
+            assert_eq!(payload.ipv4, Ipv4Addr::new(192, 168, 0, 1), "Ipv4 mismatch");
+            assert_eq!(payload.tag, String::from(tag), "Tag mismatch");
             assert_eq!(payload.val, Bytes::from(val), "Value mismatch");
         }
 
